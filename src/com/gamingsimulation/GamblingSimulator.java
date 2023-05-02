ba8bfe0fe187a081b6c9c7c0f039de24bc15484a
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-05-03 01:00:47 +0530

    Added : Code for gambler win or loose the game

--- src/com/gamingsimulation/GamblingSimulator.java
+++ src/com/gamingsimulation/GamblingSimulator.java
@@ -2,19 +2,37 @@ package com.gamingsimulation;
 
 public class GamblingSimulator {
     /*
-     * Method for initialize the stake per day and bet per game
+     * Method for gambler either win or loose the game
      * @param args
      */
 
     public static void main(String[] args) {
         /*
-         * 1) Initialize the variable and printing
+         * 1) Initialize the variable
+         * 2) Taking random variable for check either win or loose the game
+         * 3) Putting the condition if won or else loose and printing
          */
 
+        /*
+         1) Initialize the variable
+         */
         int stake_per_day = 100;
         int bet_per_game = 1;
 
-        System.out.println("Player starts game with stake of " + stake_per_day);
-        System.out.println("Player bet per game " + bet_per_game);
+        /*
+         2) Taking random variable for check either win or loose the game
+         */
+        int winLoose = (int) Math.floor(Math.random() * 2);
+
+        /*
+         3) Putting the condition if won or else loose and printing
+         */
+        if (winLoose == 1) {
+            System.out.println("You won the game");
+            System.out.println("Your stack is " + (stake_per_day + bet_per_game));
+        } else {
+            System.out.println("You loose the game");
+            System.out.println("Your stack is " + (stake_per_day - bet_per_game));
+        }
     }
 }
