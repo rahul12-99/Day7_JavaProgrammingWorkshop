9128c6339ee09b64da17c2ed440433e6f55564d2
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-05-03 00:30:34 +0530

    Added : Code for gambler starts the game

---
+++ src/com/gamingsimulation/GamblingSimulator.java
@@ -0,0 +1,20 @@
+package com.gamingsimulation;
+
+public class GamblingSimulator {
+    /*
+     * Method for initialize the stake per day and bet per game
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        /*
+         * 1) Initialize the variable and printing
+         */
+
+        int stake_per_day = 100;
+        int bet_per_game = 1;
+
+        System.out.println("Player starts game with stake of " + stake_per_day);
+        System.out.println("Player bet per game " + bet_per_game);
+    }
+}
