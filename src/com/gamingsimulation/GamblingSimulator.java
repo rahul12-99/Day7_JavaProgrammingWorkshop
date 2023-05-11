19465c11b1abcb13a87ecdbbb990180d950182cf
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-05-12 01:07:35 +0530

    Added : Code for luckiest day and unluckiest day

--- src/com/gamingsimulation/GamblingSimulator.java
+++ src/com/gamingsimulation/GamblingSimulator.java
@@ -23,22 +23,39 @@ public class GamblingSimulator {
          * 1) Initialized the variable
          * 2) Putting condition of loose or won in while loop for A month
          * 3) printing the total amount
+         * 4) Putting condition for luckiest and unluckiest day and printing
          */
         GamblingSimulator gamble = new GamblingSimulator();
         double winLoose;
         int totalDays = 30;
+        double totalWin = 0, totalloss = 0;
         int endValue = 0;
+        int win = 0, loss = 0;
         int amount = STAKE_PER_DAY;
         while (totalDays > endValue) {
             winLoose = gamble.winLoose();
             System.out.println("win or loose : " + winLoose);
             if (winLoose < 0.5) {
                 amount = amount + BET_PER_GAME;
+                win++;
+                totalWin += amount;
+                System.out.println("win");
             } else {
                 amount = amount - BET_PER_GAME;
+                loss++;
+                totalloss += amount;
+                System.out.println("loss");
             }
             totalDays--;
         }
-        System.out.println("Total amount in month = " + amount);
+        System.out.println("Amount = " + amount);
+        System.out.println("Wins = " + win + "  Loss = " + loss);
+        if (win > loss) {
+            System.out.println("won by : " + (totalWin - totalloss));
+        } else {
+            System.out.println("Lost by : " + (totalloss - totalWin));
+        }
+        System.out.println("Maximum amount won: " + totalWin + " on day " + win);
+        System.out.println("Minimum amount loss: " + totalloss + " on day " + loss);
     }
 }
